2ef66c1520e03e982d90853a68690e18fc15d118
JuHyeok <106865204+JHJuHyeok@users.noreply.github.com>
2026-09-02 15:55:46 +0900

    [Add] 유물 런타임 인스턴스 정의

---
+++ Assets/_Project/01_Scripts/Data/RelicRuntimeInstance.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+namespace OzGameLab01.Data
+{
+    /// <summary>
+    /// RelicData  : 유물의 정적 데이터
+    /// RelicLogic : 유물의 동작 로직
+    /// 
+    /// 게임 실행 중 실제 적용을 위해 정보를 묶은 런타임 인스턴스 클래스
+    /// </summary>
+    public class RelicRuntimeInstance
+    {
+        public RelicData Data { get; }
+        public RelicLogic Logic { get; }
+
+        public RelicRuntimeInstance(RelicData data)
+        {
+            Data = data;
+
+            //Logic = RelicFactory.CreateLogic(data.relicLogic);
+            Logic?.Initialize(Data, this);
+        }
+
+        public void OnEquip() => Logic?.OnEquip();
+        public void OnAttack() => Logic?.OnAttack();
+        public void OnDiceRolled() => Logic?.OnDiceRolled();
+    }
+}
