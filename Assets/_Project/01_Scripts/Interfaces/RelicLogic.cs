92314a74c89577b4f503419e841bc8b83218d231
JuHyeok <106865204+JHJuHyeok@users.noreply.github.com>
2026-09-02 15:55:25 +0900

    [Add] 유물 데이터 로직 추상 클래스 작성

---
+++ Assets/_Project/01_Scripts/Interfaces/RelicLogic.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using OzGameLab01.Data;
+
+namespace OzGameLab01.Data
+{
+    /// <summary>
+    /// 유물 로직 추상 클래스
+    /// 유물의 동작 원리 정의
+    /// </summary>
+    public abstract class RelicLogic
+    {
+        public RelicData Data { get; private set; }
+        public RelicRuntimeInstance Runtime { get; private set; }
+
+        public virtual void Initialize(RelicData data, RelicRuntimeInstance runtime)
+        {
+            Data = data;
+            Runtime = runtime;
+        }
+
+        public virtual void OnEquip() { }           // 장착 시 적용 메서드
+        public virtual void OnAttack() { }          // 전투 페이즈에서 공격 시 적용 메서드
+        public virtual void OnDiceRolled() { }      // 주사위를 굴릴 때 적용 메서드
+    }
+}
