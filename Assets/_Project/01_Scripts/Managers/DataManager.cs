57f90d535322d442fcb76b793cd35c4c04be460e
JuHyeok <106865204+JHJuHyeok@users.noreply.github.com>
2026-09-03 13:48:48 +0900

    [Add] 데이터베이스 관리 매니저 임시 작성

---
+++ Assets/_Project/01_Scripts/Managers/DataManager.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using OzGameLab01.Data;
+using System.Threading.Tasks;
+
+namespace OzGameLab01.Managers
+{
+    public static class DataManager
+    {
+        public static readonly GameDB<MonsterData, MonsterDataList> Monsters = new();
+        public static readonly GameDB<UnitData, UnitDataList> Units = new();
+        public static readonly GameDB<RelicData, RelicDataList> Relics = new();
+
+        // 어드레서블 주소 기반 모든 데이터 비동기 캐싱
+        public static async Task LoadAllDatabase()
+        {
+            await Task.WhenAll(
+                Monsters.LoadAsync(""),
+                Units.LoadAsync(""),
+                Relics.LoadAsync("")
+            );
+        }
+    }
+}
+
