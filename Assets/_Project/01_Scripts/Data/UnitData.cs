c444c03718cd78c73265760c813f74889972f497
JuHyeok <106865204+JHJuHyeok@users.noreply.github.com>
2026-09-08 14:58:17 +0900

    [Fix] 유닛 데이터 모델 수정

--- Assets/_Project/01_Scripts/Data/UnitData.cs
+++ Assets/_Project/01_Scripts/Data/UnitData.cs
@@ -2,34 +2,61 @@ using UnityEngine;
 using System.Collections.Generic;
 using OzGameLab01.Combat;
 
-public class UnitDataList : IDataList<UnitData>
+namespace OzGameLab01.Data
 {
-    public List<UnitData> unitList;
-    public List<UnitData> GetList() => unitList;
-}
+    public class UnitDataList : IDataList<UnitData>
+    {
+        public List<UnitData> unitList;
+        public List<UnitData> GetList() => unitList;
+    }
 
-[System.Serializable]
-public class UnitData
-{
-    public int id;
-    public string name;
-    public string spriteAddress;
+    [System.Serializable]
+    public class UnitData
+    {
+        public int id;
+        public string name;
+        public string spriteAddress;
 
-    public int healthPoint;
-    public int attackPoint;
-    public int criticalRate;
-    public int dodgeRate;
-    public int bloodDrain;
-    public int attackSpeed;
-    public float basicAttackCooldown = 1f;
-    public int skillCooldown;
+        public float healthPoint;       // 체력
+        public float attackPoint;       // 공격력
+        public float defensePoint;      // 방어력
+        public float attackSpeed;       // 공격 속도
+        public float criticalMult;      // 치명타 배율
+        public int criticalRate;        // 치명 확률
+        public float dodgeRate;         // 회피율
+        public float basicAttackCooldown = 1f;
 
-    public string attackKey;
-    public string skillKey;
+        public string passiveSkillKey;
+        public string activeSkillKey;
+        public int skillCooldown;
 
-    // 공용 아군 프리팹(Unit.Configure)이 생성 시 참조하는 시각/전투 클래스 정보
-    public Color color = Color.white;
-    public Unit.SkillType skillType;
+        public string attackKey;
 
-    // public UnitType type;
+        // 공용 아군 프리팹(Unit.Configure)이 생성 시 참조하는 시각/전투 클래스 정보
+        public Color color = Color.white;
+        public Unit.SkillType skillType;
+
+        public UnitTypeJob jobType;
+        public UnitTypeTribe tribeType;
+    }
+
+    public enum UnitTypeJob
+    {
+        Assasin,
+        Tricster,
+        Sage,
+        Knight,
+        Healer,
+        Shooter,
+    }
+
+    public enum UnitTypeTribe
+    {
+        Human,
+        Beast,
+        Fairy,
+        Princess,
+        Machine,
+        Kid
+    }
 }
