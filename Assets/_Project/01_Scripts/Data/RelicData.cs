2f3194fd2b3fc0e93ecb2ae194d4a96b9889d3f6
JuHyeok <106865204+JHJuHyeok@users.noreply.github.com>
2026-09-11 13:42:43 +0900

    [Fix] 유물 데이터 모델 수정

--- Assets/_Project/01_Scripts/Data/RelicData.cs
+++ Assets/_Project/01_Scripts/Data/RelicData.cs
@@ -23,8 +23,6 @@ namespace OzGameLab01.Data
         public string description;          // 유물 설명
         public string iconAddress;          // 유물 스프라이트의 어드레서블 주소
         public string relicLogic;           // 유물 로직 식별자
-
-        public int baseValue;               // 유물의 고유 수치
     }
 }
 
