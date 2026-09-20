e5333fa9debdfecb33d6d8ef12f77d4c86d9d951
JuHyeok <106865204+JHJuHyeok@users.noreply.github.com>
2026-09-20 19:00:51 +0900

    [Modify] 스킬 데이터 모델 수정

--- Assets/_Project/01_Scripts/Data/Models/SkillData.cs
+++ Assets/_Project/01_Scripts/Data/Models/SkillData.cs
@@ -9,24 +9,21 @@ namespace OzGameLab01.Data
         public List<SkillData> GetList() => skillList;
     }
 
-    [UnityEngine.Scripting.APIUpdating.MovedFrom(true, sourceNamespace: "", sourceAssembly: null, sourceClassName: null)]
     [System.Serializable]
     public class SkillData : IIdentifiable
     {
         public int id;                  // 고유 ID
-        public int unitId;              // 스킬 보유 유닛 ID
+        public int unitId;              // 연결된 유닛 ID
 
         public string name;             // 스킬 명칭
         public string description;      // 스킬 설명
-
-        public float baseValue;         // 기본 수치
-        public float subValue;          // 보조 수치(필요 값이 복수일 때)
         public float cooldown;          // 스킬 쿨타임
 
-        public TriggerType triggerType; // 스킬 발동 트리거
-        public EffectTarget target;     // 효과 대상
-        public DebuffProfile debuff;
+        // 액티브 효과 정의 (패시브일 경우 Null)
+        public List<ActiveSkillEffectNode> activeEffects = new();
+        // 패시브 효과 정의 (액티브일 경우 Null)
+        public List<EffectInstance> passiveEffects = new();
 
-        public int Id => id;
+        public int Id => id;            // GameDB 식별자
     }
 }
