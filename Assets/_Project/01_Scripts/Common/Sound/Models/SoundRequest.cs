5d991894513be19f20b040328691fcd48fe991da
iansiruna-png <iansiruna@gmail.com>
2026-09-23 14:55:05 +0900

    [Feat] 공용 사운드 재생 및 트리거 시스템 구현
    - SoundManager 기반 BGM/SFX 재생 구조 구현
    - SoundConnector를 통한 사운드 요청 흐름 구성
    - SoundTrigger를 통한 Inspector 기반 효과음 재생 지원
    - UI 버튼 클릭/호버 사운드 지원
    - 미지정 AudioClip 재생 건너뛰기 처리
    - 불필요한 ISoundConnector 인터페이스 제거

--- Assets/_Project/01_Scripts/Common/Sound/Models/SoundRequest.cs
+++ Assets/_Project/01_Scripts/Common/Sound/Models/SoundRequest.cs
@@ -1,3 +1,5 @@
+using UnityEngine;
+
 namespace OzGameLab01.Managers
 {
     /// <summary>
@@ -8,12 +10,25 @@ namespace OzGameLab01.Managers
         public SoundId Id { get; }
         public SoundChannel Channel { get; }
         public bool Restart { get; }
+        public AudioClip Clip { get; }
+        public float Volume { get; }
 
         public SoundRequest(SoundId id, SoundChannel channel, bool restart = false)
         {
             Id = id;
             Channel = channel;
             Restart = restart;
+            Clip = null;
+            Volume = 1f;
+        }
+
+        public SoundRequest(AudioClip clip, float volume)
+        {
+            Id = SoundId.None;
+            Channel = SoundChannel.Sfx;
+            Restart = false;
+            Clip = clip;
+            Volume = volume;
         }
     }
 }
